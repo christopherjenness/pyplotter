3378d6f5fb380b609a09409b5a2086a1d6af2ddc
Christopher Jenness <christopherjenness@gmail.com>
2017-05-27 08:35:00 -0400

    Fix various bugs

--- pyplotter.el
+++ pyplotter.el
@@ -1,40 +1,45 @@
 (require 'elpy)
 
-(defvar datascience-current-session)
-(defvar datascience-image-counter)
-(defvar datascience-plot-dir)
-(defvar datascience-code-name)
-(defvar datascience-code-buffer)
+(defvar pyplotter-current-session)
+(defvar pyplotter-image-counter)
+(defvar pyplotter-plot-dir)
+(defvar pyplotter-code-name)
+(defvar pyplotter-code-buffer)
 
-(defconst datascience-internal-vars '(datascience-current-session
-                                      datascience-image-counter
-                                      datascience-plot-dir
-                                      datascience-code-buffer))
+(defconst pyplotter-internal-vars '(pyplotter-current-session
+                                    pyplotter-image-counter
+                                    pyplotter-plot-dir
+                                    pyplotter-code-buffer))
 
-(dolist (var datascience-internal-vars)
-  (make-variable-buffer-local var))
+;(dolist (var pyplotter-internal-vars)
+;  (make-variable-buffer-local var))
 
 
-(defun datascience-initialize ()
+(defun plyplotter-initialize ()
   "Initialize data science IDE"
   (progn
-    (setq datascience-plot-dir (concat (buffer-file-name) "ds-plots"))
-    (ignore-errors (make-directory datascience-plot-dir))
-    (ignore-errors (image-dired datascience-plot-dir))
+    (message pyplotter-code-name)
+    (message pyplotter-plot-dir)
+    (message pyplotter-plot-dir)
+    (make-directory pyplotter-plot-dir)
+    (image-dired pyplotter-plot-dir)
     ))
 
 
-(defun datascience-update-plots ()
+(defun pyplotter-update-plots ()
   (progn
-    (let ((sw (selected-window)))
-      (set-buffer (concat datascience-code-name "ds-plots"))
+    (let ((sw (selected-window))
+          (cb (current-buffer)))
+      (set-buffer (concat pyplotter-code-name "ds-plots"))
       (revert-buffer)
-      (dired-mark-subdir-files)
+      (dired-unmark-all-marks)
+      (dired-mark-files-regexp "\.png")
       (image-dired-display-thumbs)
-      (select-window sw)))
-    (set-buffer datascience-code-buffer))
+      (select-window sw)
+      (switch-to-buffer cb)))
+    )
 
-(defun datascience-shell-send-region-or-buffer ()
+(defun pyplotter-shell-send-region-or-buffer ()
   "Send the active region or the buffer to the Python shell.
 
 Send plots to seperate buffer. Display dataframes in seperate
@@ -57,11 +62,11 @@ code is executed."
            (let ((selected-region (elpy-shell--region-without-indentation
                                    (region-beginning) (region-end))))
              (if (string-match "plt\.show.+?\)" selected-region)
-                 (setq datascience-image-counter (+ datascience-image-counter 1)))
+                 (setq pyplotter-image-counter (+ pyplotter-image-counter 1)))
              (let ((region (replace-regexp-in-string
                             "plt\.show.+?\)" (format "plt.savefig('%s/%d')"
-                                                     datascience-plot-dir
-                                                     datascience-image-counter)
+                                                     pyplotter-plot-dir
+                                                     pyplotter-image-counter)
                             selected-region)))
             (setq has-if-main (string-match if-main-regex region))
             (when (string-match "\t" region)
@@ -72,39 +77,42 @@ code is executed."
           (setq has-if-main (re-search-forward if-main-regex nil t)))
         (python-shell-send-buffer arg))
       (elpy-shell-display-buffer)
-      (datascience-update-plots)
-      (datascience-update-plots)
+      (pyplotter-update-plots)
+      (pyplotter-update-plots)      
       (when has-if-main
         (message (concat "Removed if __main__ == '__main__' construct, "
-                         "use a prefix argument to evaluate.")))))
+                         "use a prefix argument to evaluate.")))
+      (pyplotter-update-plots)))
 
 
-(define-minor-mode datascience-mode
+(define-minor-mode pyplotter-mode
   "Turn Elpy into a data science IDE"
-  :lighter " datascience"
+  :lighter " pyplotter"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c f") 'datascience-shell-send-region-or-buffer)
+            (define-key map (kbd "C-c f") 'pyplotter-shell-send-region-or-buffer)
             map)
 
-  (if datascience-mode
+  (if pyplotter-mode
       ;; Enabling.
       (progn
-        (setq datascience-image-counter 0)
-        (setq datascience-code-buffer (current-buffer))
-        (setq datascience-code-name (file-name-nondirectory (buffer-file-name)))
-        (ignore-errors datascience-initialize)
-        (set-buffer datascience-code-buffer))
+        (message "***ONE***")
+        (setq pyplotter-image-counter 0)
+        (setq pyplotter-code-name (file-name-nondirectory (buffer-file-name)))
+        (setq pyplotter-plot-dir (concat (buffer-file-name) "ds-plots"))
+        (ignore-errors make-directory pyplotter-plot-dir)
+        (let ((sw (selected-window))
+              (cb (current-buffer)))
+          (image-dired pyplotter-plot-dir)
+          (select-window sw)
+          (switch-to-buffer cb)))
 
     ;; Disabling.
     ))
 
-
-
-
 ;;;###autoload
-;;;(add-hook 'elpy-mode-hook 'datascience-mode)
+;;;(add-hook 'elpy-mode-hook 'pyplotter-mode)
 
-(provide 'datascience)
+(provide 'pyplotter)
 
 ;;; Use this to set variables
 ;;; https://www.emacswiki.org/emacs/fill-column-indicator.el
